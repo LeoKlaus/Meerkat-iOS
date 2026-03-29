27bb64f5d8a37af09f5e9c29f9d628e6fe75f4c2
Leo Wehrfritz <leo@wehrfritz.me>
2026-03-29 11:11:53 +0200

    Initial Commit

---
+++ Meerkat-iOS/Meerkat_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meerkat_iOSApp.swift
+//  Meerkat-iOS
+//
+//  Created by Leo Wehrfritz on 29.03.26.
+//
+
+import SwiftUI
+
+@main
+struct Meerkat_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
